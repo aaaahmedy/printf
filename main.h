7d1883016338bde4621a88a9ae06e60276c9b280
yassine <yassinelamnaiy@gmail.com>
2023-11-18 02:56:42 +0100

    commit

--- main.h
+++ main.h
@@ -9,5 +9,8 @@ int _putchar(char c);
 int _string_type(char *strg);
 int print_int(va_list args, char b[], int flags, int width, int precision,
 int size);
+int _int_value(int number);
+int num_len(int number);
+
 
 #endif
---
+++ my_printf.c
@@ -0,0 +1,152 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character
+ * Return: 1 (true).
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _string_type - Handle variable type string.
+ * @strg: String entered by the user
+ *  Return: number characters entered
+*/
+
+int _string_type(char *strg)
+{
+	int num_char = 0;
+
+	if (strg == NULL)
+	{
+		strg = "(null)";
+		write(1, strg, 6);
+		return (6);
+	}
+	else
+	{
+		num_char = strlen(strg);
+		write(1, strg, num_char);
+		return (num_char);
+	}
+}
+
+/**
+* numlen - calculates the lentgh of a number.
+* @number: the number given.
+* Return: the length.
+*/
+
+int num_len(int number)
+{
+	int length = 0;
+
+	while (number != 0) 
+	{
+		number /= 10;
+		length++;
+	}
+	return (length);
+}
+
+/**
+* _int_value - handling with int (%d) and (%i).
+* @number: the giving integer.
+* Return: the number printed.
+*/
+
+int _int_value(int number)
+{
+	char *string;
+	int len = 0, i;
+	if (number == 0)
+	{
+		_putchar('0');
+		return(1);
+	}
+
+	if (number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+	len = num_len(number);
+	string = (char *)malloc(sizeof(char) * (len + 1));
+	if (string == NULL)
+		return (-1);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		string[i] = '0' + (number % 10);
+		number /= 10;
+	}
+	string[len] = '\0';
+	write(1, string, len);
+	free(string);
+	return (len);
+}
+
+/**
+* _printf - produces output according to a format.
+* @format: the format specifier.
+* Return: the number of characters printed.
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0, i = 0, value = 0;
+	char *string;
+
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	while (*(format + i) != '\0')
+	{
+		if (*(format + i) == '%')
+		{
+			i++;
+			if (*(format + i) == 'c' || *(format + i) == '%')
+			{
+				value = va_arg(args, int);
+				write(1, &value, 1);
+				count++;
+			}
+			else if (*(format + i) == 's')
+			{
+				string = va_arg(args, char *);
+				value = _string_type(string);
+				count += value;
+			}
+				else if (*(format + i) == 'd' || *(format + i) == 'i')
+				{
+					value = va_arg(args, int);
+					count += _int_value(value);
+				}
+				else
+				{
+					_putchar('%');
+					if (*(format + i))
+					{
+						_putchar(*(format + i));
+						count += 2;
+					}
+				}
+		}
+		else
+		{
+			_putchar(*(format + i));
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
